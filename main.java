588d00f82252d399af469329b58e2d22df1b120b
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-06-02 17:33:49 +0600

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,20 @@
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+public class Edge<Vertex> {
+    private Vertex source;
+    private Vertex dest;
+    private Double weight;
+    public Edge(Vertex source,Vertex dest,Double weight){
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
+}
+
+private Map<Vertex,List<Edge<Vertex>>> map = new HashMap<>();
+
+public class Vertex<V>{
+    private V data;
+    private Map<Vertex<V>,Double>  adjacentVertices; 
+    public void addAdjacentVertices(Vertex<V> destination,double weight){
+       
+    }
 }
