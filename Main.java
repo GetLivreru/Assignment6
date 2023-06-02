663faa2853c012dd1c9342603b3ed3d590c0887e
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-06-02 17:50:54 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,34 @@
+import java.util.*;
+class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void setData(V data) {
+        this.data = data;
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+}
+
+
+
+
+
 public class Edge<Vertex> {
     private Vertex source;
     private Vertex dest;
