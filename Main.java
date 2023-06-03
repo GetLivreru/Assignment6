f47223d51a865d5a147113440598b30ab391effe
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-06-03 09:52:19 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -81,3 +81,9 @@ class BreadthFirstSearch<V> implements Search<V> {
         }
     }
 }
+class DijkstraSearch<V> implements Search<V> {
+    private WeightedGraph<V> graph;
+
+    public DijkstraSearch(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }
